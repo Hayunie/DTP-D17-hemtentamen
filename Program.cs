bd0f89aeeba2718c11018215bd7fca6694b03257
Julia <julia.svensson.park@gmail.com>
2023-01-13 15:12:25 +0100

    2. Förberedelse till load

--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
-﻿namespace DTP_D17_hemtentamen
+﻿using System.Data;
+
+namespace DTP_D17_hemtentamen
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filename = "file.lis";
+            Console.WriteLine("Welcome to the todo list, write 'help' for help!");
+            while (true)
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                string[] command = input.Trim().Split(' ');
+                if (command[0] == "help") 
+                {
+                    Console.WriteLine("help        - display this help text");
+                    Console.WriteLine("load /file/ - load a todo list");
+                    Console.WriteLine("quit        - quit the program");
+                }
+                else if (command[0] == "quit") 
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+                else if (command.Length >= 2 && command[0] == "load" && command[1] == filename )
+                {
+                    Console.WriteLine("NYI: load file.lis");
+                }
+                else if (command.Length >= 1 && command[0] == "load")
+                {
+                    Console.WriteLine("Usage: load /file/");
+
+
+                }
+            }
         }
     }
 }
